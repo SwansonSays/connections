a614c7ced8e357c0df40d1898839b8519a77e3b4
SwansonSays <rpswanson18@gmail.com>
2024-10-25 00:42:39 -0700

    removed test endpoints

--- server/index.js
+++ server/index.js
@@ -5,26 +5,6 @@ require('dotenv').config()
 const app  = express();
 const PORT = process.env.PORT || 8080;
 
-app.post("/post", (req, res) => {
-    console.log("Connected to React");
-    res.redirect("/");
-});
-
-app.post('/testPost', (req, res) => {
-    console.log("post test happened");
-
-    res.send({ message: 'Success'});
-});
-
-app.get('/', (req, res) => {
-    res.send('Hello from the backend!');
-});
-
-app.get('/getTest', (req, res) => {
-    console.log("\'/getTest\' endpoint used");
-    res.json({message: 'Data Data Data'});
-});
-
 app.get('/word', async (req, res) => {
     console.log('\'/word\' endpoint used');
     const word = req.query;
@@ -45,31 +25,6 @@ app.get('/word', async (req, res) => {
     } finally {
         console.log("Connection Closed")
         await client.close();
-    }
-});
-
-app.get('/top5', async (req, res) => {
-    console.log('***\'/top5\' endpoint used***');
-    //console.log(req);
-    const uri = process.env.MONGODB_URI;
-    const client = new MongoClient(uri);
-    try {
-        const database = client.db("connections");
-        const document = database.collection("words");       
-
-        const top5 = await document.find({})
-            .sort({frequency: -1})
-            .limit(5)
-            .toArray();
-
-        console.log(top5);
-        res.json(top5);
-        console.log('***\'/top5\' endpoint finished***');
-    } catch(e) {
-        console.log(e);
-    } finally {
-        console.log("Connection Closed")
-        await client.close()
     }
 });
 
@@ -345,7 +300,6 @@ app.get('/wordGraph1', async (req, res) => {
 
     res.json(graph);
 });
-
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
